bc16319d9be501b55b61865780cd41d84b31a748
GalalMohammed <galalmoh2001@gmail.com>
2023-12-07 15:56:31 +0200

    add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module defines add function.
+
+Example:
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+
+"""
+
+
+def add(a: float, b: float) -> float:
+    """type-annotated function add.
+
+    Args:
+        a (float): first arg.
+        b (float): second arg.
+
+    Returns:
+        their sum.
+
+    """
+    return a + b
