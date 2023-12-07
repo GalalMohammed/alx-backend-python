58a6e0f31471de5cc61b186ab2bfa8bb4582a543
GalalMohammed <galalmoh2001@gmail.com>
2023-12-07 16:04:38 +0200

    add task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module defines floor function.
+
+Example:
+    ans = floor(3.14)
+
+"""
+
+
+def floor(n: float) -> float:
+    """type-annotated function floor.
+
+    Args:
+        n (float):  number.
+
+    Returns:
+        floored float.
+
+    """
+    return int(n)
