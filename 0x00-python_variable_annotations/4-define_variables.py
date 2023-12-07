ae21f75756ff96152478b6f7485f71650e620711
GalalMohammed <galalmoh2001@gmail.com>
2023-12-07 16:15:40 +0200

    add task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module defines variables.
+
+Example:
+    print("a is a {} with a value of {}".format(type(a), a))
+
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
