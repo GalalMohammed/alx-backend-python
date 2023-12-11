c50532276abaffd8fc228d846c48a0ba896f33ac
GalalMohammed <galalmoh2001@gmail.com>
2023-12-11 12:54:27 +0200

    add task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module defines wait_random function.
+
+Example:
+    print(asyncio.run(wait_random()))
+
+"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """Wait for a random delay.
+
+    Args:
+        max_delay (float): included arg.
+
+    Returns:
+        float.
+
+    """
+    async_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(async_delay)
+    return async_delay
