760faf66fb3e06c8e539752d55a04d2ad7553ffa
GalalMohammed <galalmoh2001@gmail.com>
2024-01-27 01:54:51 +0200

    add task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ This module defines unit test class for client.
 """
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -25,8 +25,17 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         client = GithubOrgClient(org)
         client.org()
-        client.org()
         mock_method.assert_called_once_with(org_url)
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock,
+           return_value={
+                'repos_url': "https://api.github.com/orgs/example_org/repos"})
+    def test_public_repos_url(self, _: MagicMock) -> None:
+        """Test _public_repos_url.
+        """
+        client = GithubOrgClient("example")
+        self.assertEqual(client._public_repos_url,
+                         "https://api.github.com/orgs/example_org/repos")
 
 
 if __name__ == "__main__":
